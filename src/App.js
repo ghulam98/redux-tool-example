2065daf9375db214bc93f3067364863f875805a7
Ghulam Mohiyuddin <ghulam.178204@knit.ac.in>
2023-03-15 22:20:05 +0530

    removed all unused props

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import { useSelector } from 'react-redux';
 import './App.css';
 import Home from './component/Home';
 function App() {
-  const [count, setCount] = useState(0)
+ 
   const {c} = useSelector(state=>state.custom)
 
   return (
     <>
     <h3>inside App component Counter: {c}</h3>
-    <Home count={count} setCount={setCount}/>
+    <Home />
     </>
   );
 }
